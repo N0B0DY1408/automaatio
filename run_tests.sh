caac008cfa29a76c07cf732c840cbe39d571d1a1
N0B0DY1408 <tuikkamatias@gmail.com>
2025-12-09 16:19:05 +0200

    Create run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+# #!/bin/bash
+#set -e
+
+# Lisää tähän omat testikomennot, esim:
+# pytest
+# make test
+# dotnet test
+# cargo test
+
+#echo "Running tests..."
+#echo "(Tässä voisi ajaa projektin omat testit.)"
+
+# Jos testejä ei ole:
+#!/bin/bash
+echo "No tests, skipping."
